68c69cb115fb1318b150d765b554b564b2ffd1a6
Diones <pereiradiones987@gmail.com>
2023-06-07 21:05:15 -0300

    adicionando comentário no newsController

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,20 +1,27 @@
 
+//Esse método rendenriza a tela inicia do site 
+// e é também onde terá a listagem de todas das notícias aprovadas
 const home = (req, res) => {
   res.render("pages/news/home.ejs");
 }
 
+//Esse método redenriza os detalhes da notícia
 const newsDetail = (req, res) => {
   res.render("pages/news/newsDetail.ejs");
 }
 
+//Esse método rendenriza uma nova tela com as últimas notícias cadastrada
+//da notícia mais atual até a mais antiga
 const lastedNews = (req, res) => {
   res.render("pages/news/lastedNews.ejs");
 }
 
+//Redenriza a tela sobre
 const about = (req, res) => {
   res.render("pages/news/about.ejs");
 }
 
+//Renderiza a tela de contato
 const contact = (req, res) => {
   res.render("pages/news/contact.ejs");
 }
