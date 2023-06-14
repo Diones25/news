8657c02da308f3c854a6fa48e7b8d8c86dd34d42
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2023-06-14 03:58:40 -0300

    Create the AuthController.test.js

---
+++ test/unit/AuthController.test.js
@@ -0,0 +1,55 @@
+const authController = require('../../src/controllers/AuthController');
+const User = require('../../src/models/User.js');
+
+jest.mock('../../src/models/User.js', () => ({
+  signin: jest.fn(),
+  signup: jest.fn(),
+}));
+
+describe('Controlador de Autenticação', () => {
+  describe('signin()', () => {
+    it('deve renderizar a página de login', () => {
+      const req = {};
+      const res = {
+        render: jest.fn(),
+      };
+
+      authController.signin(req, res);
+
+      expect(res.render).toHaveBeenCalledWith('pages/admin/signin.ejs');
+    });
+  });
+
+  describe('signup()', () => {
+    it('deve renderizar a página de cadastro de login', () => {
+      const req = {};
+      const res = {
+        render: jest.fn(),
+      };
+
+      authController.signup(req, res);
+
+      expect(res.render).toHaveBeenCalledWith('pages/admin/signup.ejs');
+    });
+  });
+
+  describe('logout()', () => {
+    it('deve destruir a sessão e redirecionar para a página de login', () => {
+      const req = {
+        session: {
+          destroy: jest.fn(),
+        },
+      };
+      const res = {
+        redirect: jest.fn(),
+      };
+
+      authController.logout(req, res);
+
+      expect(req.session.destroy).toHaveBeenCalled();
+      expect(res.redirect).toHaveBeenCalledWith('/signin');
+    });
+  });
+});
+
+  
